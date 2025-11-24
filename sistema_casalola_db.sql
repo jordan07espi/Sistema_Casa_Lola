5a4d3b74f4e93e1e15252e8f33f7609f16a72472
jordan07espi <jeespinosa5@espe.edu.ec>
2025-11-24 17:00:53 -0500

    Base de datos

---
+++ sistema_casalola_db.sql
@@ -0,0 +1,359 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: db
+-- Tiempo de generación: 24-11-2025 a las 22:00:26
+-- Versión del servidor: 8.4.6
+-- Versión de PHP: 8.2.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `sistema_casalola_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id_cliente` int NOT NULL,
+  `cedula` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `nombre` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
+  `telefono` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT '1',
+  `fecha_registro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`id_cliente`, `cedula`, `nombre`, `telefono`, `activo`, `fecha_registro`) VALUES
+(1, '2350637217', 'JORDAN ESPINOSA', '0986780565', 0, '2025-11-24 18:30:41'),
+(2, '1714309844', 'LUIS ESPINOSA', '0986780565', 1, '2025-11-24 18:30:58'),
+(3, '2350637217', 'JORDAN ESPINOSA', '0986780565', 1, '2025-11-24 18:39:33'),
+(4, '2300533193', 'HECTOR CEDEÑO', '0985134893', 1, '2025-11-24 21:30:01');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalles_pedido`
+--
+
+CREATE TABLE `detalles_pedido` (
+  `id_detalle` int NOT NULL,
+  `id_pedido` int NOT NULL,
+  `id_producto` int NOT NULL,
+  `cantidad` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `detalles_pedido`
+--
+
+INSERT INTO `detalles_pedido` (`id_detalle`, `id_pedido`, `id_producto`, `cantidad`) VALUES
+(1, 1, 1, 1),
+(9, 3, 4, 1),
+(10, 3, 5, 2),
+(11, 4, 1, 1),
+(12, 4, 2, 2),
+(13, 4, 3, 1),
+(14, 4, 6, 3),
+(15, 5, 1, 1),
+(16, 5, 5, 2),
+(17, 5, 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `intentos_login_fallidos`
+--
+
+CREATE TABLE `intentos_login_fallidos` (
+  `id` int NOT NULL,
+  `cedula` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `ip_address` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `fecha_intento` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedidos`
+--
+
+CREATE TABLE `pedidos` (
+  `id_pedido` int NOT NULL,
+  `codigo_pedido` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `id_cliente` int NOT NULL,
+  `id_usuario` int NOT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `fecha_entrega` date NOT NULL,
+  `hora_entrega` time NOT NULL,
+  `total` decimal(10,2) NOT NULL,
+  `estado` enum('Pendiente','Entregado','Cancelado') COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'Pendiente',
+  `observaciones` text COLLATE utf8mb4_general_ci,
+  `evidencia_foto` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `pedidos`
+--
+
+INSERT INTO `pedidos` (`id_pedido`, `codigo_pedido`, `id_cliente`, `id_usuario`, `fecha_creacion`, `fecha_entrega`, `hora_entrega`, `total`, `estado`, `observaciones`, `evidencia_foto`) VALUES
+(1, '2025_45', 3, 1, '2025-11-24 20:36:59', '2025-11-29', '07:00:00', 160.00, 'Entregado', 'Maduro', NULL),
+(3, '2025_FF', 4, 1, '2025-11-24 21:36:27', '2025-11-30', '07:00:00', 60.00, 'Cancelado', 'Vienen con bandeja', NULL),
+(4, '2025_24', 4, 1, '2025-11-24 21:52:02', '2025-11-24', '21:50:00', 200.00, 'Pendiente', 'Viene maduro', NULL),
+(5, '2025_34', 3, 1, '2025-11-24 21:57:11', '2025-11-24', '21:55:00', 45.00, 'Pendiente', '', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedidos_tillos`
+--
+
+CREATE TABLE `pedidos_tillos` (
+  `id_tillo` int NOT NULL,
+  `id_pedido` int NOT NULL,
+  `codigo_tillo` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `estado` enum('Pendiente','Entregado') COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'Pendiente'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `pedidos_tillos`
+--
+
+INSERT INTO `pedidos_tillos` (`id_tillo`, `id_pedido`, `codigo_tillo`, `estado`) VALUES
+(1, 4, '2025_24', 'Pendiente'),
+(2, 4, '2025_FF', 'Pendiente'),
+(3, 4, '2025_25', 'Pendiente'),
+(4, 5, '2025_34', 'Pendiente'),
+(5, 5, '2025_45', 'Pendiente'),
+(6, 5, '2025_67', 'Pendiente');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id_producto` int NOT NULL,
+  `nombre_producto` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT '1',
+  `requiere_tillo` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id_producto`, `nombre_producto`, `activo`, `requiere_tillo`) VALUES
+(1, 'Chancho', 1, 1),
+(2, 'Costilla', 1, 1),
+(3, 'Tortillas', 1, 0),
+(4, 'Piernas', 1, 1),
+(5, 'Pavo', 1, 1),
+(6, 'Agrio', 1, 0),
+(7, 'Brazos', 1, 1),
+(8, 'Pollos', 1, 1),
+(9, 'Motes', 1, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id_rol` int NOT NULL,
+  `nombre_rol` varchar(50) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id_rol`, `nombre_rol`) VALUES
+(1, 'Administrador'),
+(2, 'Empleado');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int NOT NULL,
+  `nombre_completo` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
+  `cedula` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `id_rol` int NOT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `nombre_completo`, `cedula`, `password`, `id_rol`, `activo`) VALUES
+(1, 'Jordan Espinosa', '2350637217', '$2y$10$VWjzn/QJiNd/XhUC/E5TSOA2P./VddqiuVlxtD7zOEXbxiZ/iHRL6', 1, 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id_cliente`);
+
+--
+-- Indices de la tabla `detalles_pedido`
+--
+ALTER TABLE `detalles_pedido`
+  ADD PRIMARY KEY (`id_detalle`),
+  ADD KEY `fk_detalles_pedido` (`id_pedido`),
+  ADD KEY `fk_detalles_producto` (`id_producto`);
+
+--
+-- Indices de la tabla `intentos_login_fallidos`
+--
+ALTER TABLE `intentos_login_fallidos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`id_pedido`),
+  ADD KEY `fk_pedidos_cliente` (`id_cliente`),
+  ADD KEY `fk_pedidos_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `pedidos_tillos`
+--
+ALTER TABLE `pedidos_tillos`
+  ADD PRIMARY KEY (`id_tillo`),
+  ADD KEY `fk_tillos_pedido` (`id_pedido`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id_producto`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id_rol`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `idx_cedula_unica` (`cedula`),
+  ADD KEY `fk_usuarios_roles_idx` (`id_rol`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id_cliente` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `detalles_pedido`
+--
+ALTER TABLE `detalles_pedido`
+  MODIFY `id_detalle` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT de la tabla `intentos_login_fallidos`
+--
+ALTER TABLE `intentos_login_fallidos`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  MODIFY `id_pedido` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `pedidos_tillos`
+--
+ALTER TABLE `pedidos_tillos`
+  MODIFY `id_tillo` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id_producto` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id_rol` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detalles_pedido`
+--
+ALTER TABLE `detalles_pedido`
+  ADD CONSTRAINT `fk_detalles_pedido` FOREIGN KEY (`id_pedido`) REFERENCES `pedidos` (`id_pedido`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_detalles_producto` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`);
+
+--
+-- Filtros para la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD CONSTRAINT `fk_pedidos_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id_cliente`),
+  ADD CONSTRAINT `fk_pedidos_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`);
+
+--
+-- Filtros para la tabla `pedidos_tillos`
+--
+ALTER TABLE `pedidos_tillos`
+  ADD CONSTRAINT `fk_tillos_pedido` FOREIGN KEY (`id_pedido`) REFERENCES `pedidos` (`id_pedido`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `fk_usuarios_roles` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id_rol`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
